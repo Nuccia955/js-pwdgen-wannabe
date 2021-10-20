33efffc85a1e690ffb893a3779e883ef5506f281
Nuccia955 <elenamunaro1995@gmail.com>
2021-10-20 14:23:27 +0200

    sketch task password generator

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log("test")
+/* TASK PASSWORD GENERATOR */
+
+// 1. chiedi nome utente
+//    -dichiara variabile_assegna valore con prompt;
+
+// 2. chiedi cognome utente
+//    -dichiara variabile_assegna valore con prompt;
+
+// 3. chiedi colore preferito utente
+//    -dichiara variabile_assegna valore con prompt;
+
+// 4. stampa a video: NomeCognomeColore21
+//    -seleziona elemento in HTML con id_aggiungi proprietà_assegna valore;
